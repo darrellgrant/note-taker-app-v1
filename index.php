85145ab237beec740fe57afb7ee08d4aa372adea
Darrell Grant <dlsan3000@gmail.com>
2023-04-02 17:42:18 -0400

    modified files
    	modified:   index.php
    :wq

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Note Taker</title>
+    <title>Blogging App</title>
   </head>
   <body>
     <div class="user_post">
